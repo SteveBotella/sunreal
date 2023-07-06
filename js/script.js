0df8ae68a2b570b7fa3441756f028809a7baf9e5
Steve Botella <stevebotella@hotmail.fr>
2023-07-06 20:52:41 +0200

    feat: Navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-﻿
+﻿// Run only when document ready
+$(document).ready(function () {
+    $(function () {
+        var link = $("nav a");
+        var linkimg = $("nav img");
+        //click handler
+        link.on("click", function () {
+            var $this = $(this);
+            var page = $this.data("page");
+
+            $("body").removeClass().addClass(page);
+            linkimg.removeClass("active");
+            link.removeClass("active");
+            $this.addClass("active");
+        })
+
+        linkimg.on("click", function () {
+            var $this = $(this);
+            var page = $this.data("page");
+
+            $("body").removeClass().addClass(page);
+            linkimg.removeClass("active");
+            link.removeClass("active");
+            $this.addClass("active");
+        })
+    });
+})

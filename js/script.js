9f92dd6e5b2d1d8736bbad236d871cac7dca600b
Steve Botella <stevebotella@hotmail.fr>
2023-07-06 22:21:56 +0200

    refactoring navbar

--- js/script.js
+++ js/script.js
@@ -2,24 +2,12 @@
 $(document).ready(function () {
     $(function () {
         var link = $("nav a");
-        var linkimg = $("nav img");
         //click handler
         link.on("click", function () {
             var $this = $(this);
             var page = $this.data("page");
 
             $("body").removeClass().addClass(page);
-            linkimg.removeClass("active");
-            link.removeClass("active");
-            $this.addClass("active");
-        })
-
-        linkimg.on("click", function () {
-            var $this = $(this);
-            var page = $this.data("page");
-
-            $("body").removeClass().addClass(page);
-            linkimg.removeClass("active");
             link.removeClass("active");
             $this.addClass("active");
         })

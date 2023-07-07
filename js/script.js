d38882fa9ac8b876b35d6a91b41b6ee9063f1fe0
Steve Botella <stevebotella@hotmail.fr>
2023-07-07 03:16:24 +0200

    feat: navigation template

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
 ﻿// Run only when document ready
 $(document).ready(function () {
-    // Enable scrollbar
-    $("body").css({ "overflow": "visible" })
-
-    let link = $("nav a");
-    //click handler
-    link.on("click", function () {
-        var $this = $(this);
-        var page = $this.data("page");
-
-        $("body").removeClass().addClass(page);
-        link.removeClass("active");
-        $this.addClass("active");
-    });
+    clearChildren("loaded");
+    if (document.body.classList.contains("home")) {
+        addVideo();
+    }
 
     // Clear the div you want
     function clearChildren(id) {
@@ -20,22 +11,73 @@ $(document).ready(function () {
         remove.innerHTML = '';
     }
 
-    // --- EVENTS ---
-    // Button Home
+    // Enable scrollbar
+    $("body").css({ "overflow": "visible" })
 
-    $("#sunrealLogo").on("click", function () {
-        clearChildren("loaded");
-        //Add Video
+    //Add Video
+    function addVideo() {
+        let loaded = document.getElementById("loaded");
         let newYTVideo = document.createElement("div");
         let newYTVideoIframe = document.createElement("iframe");
 
         newYTVideo.id = "headerVideo";
 
         newYTVideoIframe.classList.add("video");
-        newYTVideoIframe.src = "https://www.youtube.com/embed/cLaoQaIf7rs?mute=1";
+        newYTVideoIframe.src = "https://www.youtube.com/embed/cLaoQaIf7rs?autoplay=1&mute=1";
 
         newYTVideo.appendChild(newYTVideoIframe);
 
         loaded.appendChild(newYTVideo);
+    }
+
+    // Change active page
+    function requireWhenClickNav(id) {
+        let link = $("nav a");
+        //click handler
+        let $this = $("#" + id);
+        let page = $this.data("page");
+
+        $("body").removeClass().addClass(page);
+        link.removeClass("active");
+        $this.addClass("active");
+    }
+
+    // --- EVENTS ---
+    // Button Home
+
+    $("#sunrealLogo").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("sunreal");
+        addVideo();
+    });
+
+    $("#news").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("news");
+    });
+
+    $("#marketplace").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("marketplace");
+    });
+
+    $("#tutorials").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("tutorials");
+    });
+
+    $("#twitchLogo").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("twitch");
+    });
+
+    $("#ytLogo").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("youtube");
+    });
+
+    $("#linkedinLogo").on("click", function () {
+        clearChildren("loaded");
+        requireWhenClickNav("linkedin");
     });
 })

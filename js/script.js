07bf44bddec96903f65028f3d293bfea3ecc7aea
Steve Botella <stevebotella@hotmail.fr>
2023-08-15 14:50:23 +0200

    feat: Add button link

--- js/script.js
+++ js/script.js
@@ -83,28 +83,50 @@ $(document).ready(function () {
         $this.addClass("active");
     }
 
-    // Add text with optional title
-    function addText(title, text, link, linkName) {
+    // Add text with optional title & link
+    function addText(title, text, link = "", linkName = "Lien") {
         let loaded = document.getElementById("loaded");
         let textArea = document.createElement("div");
         let titleElement = document.createElement("h2");
         let textElement = document.createElement("p");
-        let linkElement = document.createElement("a");
 
         loaded.appendChild(textArea);
         textArea.appendChild(titleElement);
         textArea.appendChild(textElement);
-        textArea.appendChild(linkElement);
+        textArea.appendChild(textElement);
 
         textArea.classList.add("textarea");
 
         titleElement.textContent += title;
         textElement.textContent += text;
-        linkElement.textContent += linkName;
-        linkElement.href = link;
-        
 
+        if (link !== "") {
+            buttonLink(link, linkName, textArea);
+        }
+    }
 
+    // Button link
+    function buttonLink(btnLink, btnTitle, slot) {
+        let divbtnconteiner = document.createElement("div");
+        let abtncontent = document.createElement("a");
+        let spanbtntitle = document.createElement("span");
+        let spaniconarrow = document.createElement("span");
+        let img = document.createElement("img");
+
+        slot.appendChild(divbtnconteiner);
+        divbtnconteiner.appendChild(abtncontent);
+        abtncontent.appendChild(spanbtntitle);
+        abtncontent.appendChild(spaniconarrow);
+        spaniconarrow.appendChild(img);
+
+        divbtnconteiner.classList.add("btn-conteiner");
+        abtncontent.classList.add("btn-content");
+        abtncontent.href = btnLink;
+        abtncontent.target = "_blank";
+        spanbtntitle.classList.add("btn-title");
+        spanbtntitle.textContent = btnTitle;
+        img.classList.add("btnarrow");
+        img.src = "img/right-arrow.png";
     }
 
     // --- EVENTS ---
